5f645bac23d59189329b3e438a9dc3654aa16ce2
Camilo Aguilar <camilo.aguilar@gmail.com>
2016-11-01 11:48:08 -0400

    Goes back to using AWS official SDK.
    So, streaming up multipart request is possible with AWS official SDK.
    
    Also, on my tests, memory usage with s3gof3r kept going up.
    With AWS's SDK it was stable at 139mb each time I tested.

--- server/web/files/s3.go
+++ server/web/files/s3.go
@@ -4,25 +4,35 @@ import (
 	"io"
 	"mime/multipart"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+
 	"github.com/hooklift/lift-registry/server/config"
 	"github.com/pkg/errors"
-	"github.com/rlmcpherson/s3gof3r"
 )
 
 // S3 implements the storage driver for AWS S3.
 type S3 struct {
-	bucket *s3gof3r.Bucket
+	uploader   *s3manager.Uploader
+	downloader *s3.S3
 }
 
 // NewS3 returns a new instance of an S3 storage provider.
 func NewS3() StorageProvider {
-	k, err := s3gof3r.EnvKeys() // get S3 keys from environment
+	sess, err := session.NewSession()
 	if err != nil {
 		panic(err)
 	}
 
-	s3 := s3gof3r.New(s3gof3r.DefaultDomain, k)
-	return &S3{bucket: s3.Bucket(config.S3Bucket)}
+	uploader := s3manager.NewUploader(sess)
+	downloader := s3.New(sess)
+
+	return &S3{
+		uploader:   uploader,
+		downloader: downloader,
+	}
 }
 
 // Upload uploads file parts to S3 as they arrive from the client.
@@ -39,17 +49,15 @@ func (s *S3) Upload(reader *multipart.Reader) error {
 			continue
 		}
 
-		w, err := s.bucket.PutWriter(fileName, nil, s3gof3r.DefaultConfig)
+		input := &s3manager.UploadInput{
+			Bucket: aws.String(config.S3Bucket),
+			Key:    aws.String(fileName),
+			Body:   part,
+		}
+
+		_, err = s.uploader.Upload(input)
 		if err != nil {
-			return errors.Wrapf(err, "failed initializing multipart request for %q", fileName)
-		}
-
-		if _, err = io.Copy(w, part); err != nil {
-			return errors.Wrapf(err, "failed writing %q to S3", fileName)
-		}
-
-		if err = w.Close(); err != nil {
-			return errors.Wrapf(err, "failed closing put writer for %q", fileName)
+			return errors.Wrapf(err, "failed uploading %q to S3", fileName)
 		}
 	}
 
@@ -59,10 +67,14 @@ func (s *S3) Upload(reader *multipart.Reader) error {
 // Get streams down a package file from S3.
 // The caller must close the reader once it finishes reading from it.
 func (s *S3) Get(key string) (io.ReadCloser, error) {
-	r, _, err := s.bucket.GetReader(key, nil)
+	result, err := s.downloader.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(config.S3Bucket),
+		Key:    aws.String(key),
+	})
+
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed getting reader to download %q from S3", key)
+		return nil, errors.Wrapf(err, "failed downloading %q from S3", key)
 	}
 
-	return r, nil
+	return result.Body, nil
 }
