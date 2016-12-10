942d6211c3b812a07dfa6c899e8438506679fc46
Camilo Aguilar <camilo.aguilar@gmail.com>
2016-12-10 09:22:14 -0500

    Cleans up code a bit

--- server/config/config.go
+++ server/config/config.go
@@ -69,47 +69,14 @@ func Read() {
 	// openssl req -new -x509 -key cert-key.pem -out cert.pem -days 1920
 	TLSCert = os.Getenv("TLS_CERT")
 	if TLSCert == "" {
-		log.Printf("[WARN] !!! UNSAFE: No TLS certificate configured, using default TLS certificate !!!")
-		TLSCert = `
------BEGIN CERTIFICATE-----
-MIIDUzCCAtmgAwIBAgIJAKTf/aVGhWkYMAkGByqGSM49BAEwgZExCzAJBgNVBAYT
-AlVTMREwDwYDVQQIEwhOZXcgWW9yazERMA8GA1UEBxMITmV3IFlvcmsxFzAVBgNV
-BAoTDkhvb2tsaWZ0LCBJbmMuMRQwEgYDVQQLEwtFbmdpbmVlcmluZzEKMAgGA1UE
-AxQBKjEhMB8GCSqGSIb3DQEJARYSY2FtaWxvQGhvb2tsaWZ0LmlvMCAXDTE2MTEx
-NDE0NTgwNFoYDzIxMTUwNjA5MTQ1ODA0WjCBkTELMAkGA1UEBhMCVVMxETAPBgNV
-BAgTCE5ldyBZb3JrMREwDwYDVQQHEwhOZXcgWW9yazEXMBUGA1UEChMOSG9va2xp
-ZnQsIEluYy4xFDASBgNVBAsTC0VuZ2luZWVyaW5nMQowCAYDVQQDFAEqMSEwHwYJ
-KoZIhvcNAQkBFhJjYW1pbG9AaG9va2xpZnQuaW8wdjAQBgcqhkjOPQIBBgUrgQQA
-IgNiAASH3bmfhqPNDE2YdeBG15Yl13GVWlex0QDCh85koZ3kbKMGdDBqgb5gqgwZ
-F1rCCpjff+o3D3JaAMYosACOyHn8lnJOcpryqUkwCklxSQqleLJM4EGSitMm8119
-tzYhaCajgfkwgfYwHQYDVR0OBBYEFMNqnVpZOU6jIqWaiHr7AnMXpBwWMIHGBgNV
-HSMEgb4wgbuAFMNqnVpZOU6jIqWaiHr7AnMXpBwWoYGXpIGUMIGRMQswCQYDVQQG
-EwJVUzERMA8GA1UECBMITmV3IFlvcmsxETAPBgNVBAcTCE5ldyBZb3JrMRcwFQYD
-VQQKEw5Ib29rbGlmdCwgSW5jLjEUMBIGA1UECxMLRW5naW5lZXJpbmcxCjAIBgNV
-BAMUASoxITAfBgkqhkiG9w0BCQEWEmNhbWlsb0Bob29rbGlmdC5pb4IJAKTf/aVG
-hWkYMAwGA1UdEwQFMAMBAf8wCQYHKoZIzj0EAQNpADBmAjEAnvDrqcg7Sl2wK/bH
-+98IMGMiYdT1FpSqCT3YyVQeCPELlxmnXbzNesY/R+l8oY9bAjEAhya4BL+ingli
-o9FuJqdUS5o9Rgii55nFhNdzQvT/p/ANGHBCfQyUNtAjPp92KvXC
------END CERTIFICATE-----
-		`
+		log.Fatal("TLS_CERT config variable must be set")
 	}
 
 	// For development purposes, use the following to regenerate key:
 	// openssl ecparam -genkey -name secp384r1 -out cert-key.pem
 	TLSKey = os.Getenv("TLS_KEY")
 	if TLSKey == "" {
-		log.Printf("[WARN] !!! UNSAFE: No TLS private key configured, using default TLS private key !!!")
-		TLSKey = `
------BEGIN EC PARAMETERS-----
-BgUrgQQAIg==
------END EC PARAMETERS-----
------BEGIN EC PRIVATE KEY-----
-MIGkAgEBBDAD5WLfofxxT8EewIU/VYJ5hXRWAyjxwAhemboTVnnrCmqA5Icxz+oa
-kVluFU7LiPWgBwYFK4EEACKhZANiAASH3bmfhqPNDE2YdeBG15Yl13GVWlex0QDC
-h85koZ3kbKMGdDBqgb5gqgwZF1rCCpjff+o3D3JaAMYosACOyHn8lnJOcpryqUkw
-CklxSQqleLJM4EGSitMm8119tzYhaCY=
------END EC PRIVATE KEY-----
-		`
+		log.Fatal("TLS_KEY config variable must be set")
 	}
 
 	ClientURI = os.Getenv("OAUTH2_CLIENT_URI")
